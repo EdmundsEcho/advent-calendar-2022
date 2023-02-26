0f9fc1fbc4c77574a019468d9167216f866967ec
Edmund Cape <edmund@Edmunds-MacBook-Pro.local>
2023-02-26 12:50:20 -0500

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::include_str;
 
+// top 3
 pub fn main() {
-    println!(
-        "{:?}",
-        include_str!("input.txt")
-            .split("\n\n")
-            .map(|elf| { elf.lines().map(|n| n.parse::<u32>().unwrap()).sum::<u32>() })
-            .max()
-    );
+    let mut vec = include_str!("input.txt")
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|n| n.parse::<u32>().unwrap()).sum::<u32>())
+        .collect::<Vec<u32>>();
+
+    vec.sort_by(|a, b| b.cmp(a));
+
+    let temp: u32 = vec.iter().take(3).sum();
+
+    println!("{:?}", temp);
 }
