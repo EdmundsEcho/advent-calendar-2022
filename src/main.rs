a38916006116a0416f2767ec0965d13f901a797a
Edmund Cape <edmund@Edmunds-MacBook-Pro.local>
2023-02-27 19:07:17 -0500

    day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::include_str;
-
+// split the input in half
+// find matching values in each side
+// compute the priority of each
 pub fn main() {
-    println!(
-        "{:?}",
-        include_str!("input.txt")
-            .split("\n\n")
-            .map(|elf| { elf.lines().map(|n| n.parse::<u32>().unwrap()).sum::<u32>() })
-            .max()
-    );
+    let result = include_bytes!("input.txt")
+        .split(|b| *b == b'\n')
+        .map(|sack| sack.split_at(sack.len() / 2))
+        .map(|(a, b)| b.iter().filter(|b| a.contains(b)).collect::<Vec<_>>())
+        .filter(|matches| !matches.is_empty())
+        .map(|matches| {
+            let b = matches[0];
+            if *b >= b'a' {
+                (b - b'a') as u32 + 1
+            } else {
+                (b - b'A') as u32 + 27
+            }
+        })
+        .sum::<u32>();
+
+    println!("a: {}", b'a');
+    println!("A: {}", b'A');
+    println!("Answer: {}", result);
 }
